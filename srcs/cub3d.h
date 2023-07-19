18f89fb87017d16c56017bca082d8ccb767a0b06
임원정 <wonlim@student.42seoul.kr>
2023-07-19 21:21:56 +0900

    add default src file

---
+++ srcs/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 21:16:45 by wonlim            #+#    #+#             */
+/*   Updated: 2023/07/19 21:19:33 by wonlim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+# include "../libft/libft.h"
+# include "../mlx/mlx.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
