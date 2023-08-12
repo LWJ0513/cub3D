426776fe0460458b76852d6c9bea9a73c3980f11
임원정 <wonlim@student.42seoul.kr>
2023-08-13 02:49:31 +0900

    소문자로 변환

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -6,143 +6,138 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 21:16:45 by wonlim            #+#    #+#             */
-/*   Updated: 2023/08/08 21:13:32 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/08/13 02:45:12 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-# define CUB3D_H
+#define CUB3D_H
 
-# include "../libft/libft.h"
-# include "../mlx/mlx.h"
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <math.h>
+#include "../libft/libft.h"
+#include "../mlx/mlx.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-# define WID 640
-# define HEI 480
-# define NO 0
-# define SO 1
-# define WE 2
-# define EA 3
-# define F 4
-# define C 5 
+#define WID 640
+#define HEI 480
+#define NO 0
+#define SO 1
+#define WE 2
+#define EA 3
+#define F 4
+#define C 5
 
-# define X_EVENT_KEY_PRESS 2
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_ESC 53
+#define X_EVENT_KEY_PRESS 2
+#define KEY_W 13
+#define KEY_A 0
+#define KEY_S 1
+#define KEY_D 2
+#define KEY_ESC 53
 
-typedef struct s_image {
-	int		width;
-	int		height;
-	int		size_line;
-	int		bits_per_pixel;
-	int		endian;
-	int		*buffer;
-	void	*img;
-}	t_image;
-
-typedef struct s_data {
-	void	*mlx;
-	void	*win;
-	t_image	*images;
-	int		*rgb_ceil_color;
-	int		*rgb_floor_color;
-	int		ceil_color;
-	int		floor_color;
-	char	direction;
-	int		width;
-	int		height;
-	char	**map;
-
-	double	pos_x;
-	double	pos_y;
-	double	dir_x;
-	double	dir_y;
-	double	plane_x;
-	double	plane_y;
-	double	move_speed;
-	double	rot_speed;
-	t_image	image;
-}	t_data;
-
-typedef struct s_parsing {
-	char	*file_path;
-	int		fd;
-	int		identifier_count;
-	int		*identifier_list;
-	int		is_empty;
-	char	*start_line;
-	char	**image_path;
-}	t_parsing;
-
-// typedef struct s_var
-// {
-// 	double	camera_x;
-// 	double	raydir_x;
-// 	double	raydir_y;
-// 	int		map_x;
-// 	int		map_y;
-// 	double	side_x;
-// 	double	side_y;
-// 	double	delta_x;
-// 	double	delta_y;
-// 	int		step_x;
-// 	int		step_y;
-
-// 	int		side;
-// 	double	perp_wall_dist;
-// 	int		wall_height;
-// 	int		draw_start;
-// 	int		draw_end;
-// }	t_var;
-
-typedef struct var
+typedef struct s_image
 {
-	double wallX;
+	int width;
+	int height;
+	int size_line;
+	int bits_per_pixel;
+	int endian;
+	int *buffer;
+	void *img;
+} t_image;
+
+typedef struct s_data
+{
+	void *mlx;
+	void *win;
+	t_image *images;
+	int *rgb_ceil_color;
+	int *rgb_floor_color;
+	int ceil_color;
+	int floor_color;
+	char direction;
+	int width;
+	int height;
+	char **map;
+
+	double pos_x;
+	double pos_y;
+	double dir_x;
+	double dir_y;
+	double plane_x;
+	double plane_y;
+	double move_speed;
+	double rot_speed;
+	t_image image;
+} t_data;
+
+typedef struct s_parsing
+{
+	char *file_path;
+	int fd;
+	int identifier_count;
+	int *identifier_list;
+	int is_empty;
+	char *start_line;
+	char **image_path;
+} t_parsing;
+
+typedef struct s_var
+{
+	double camera_x;
+	double raydir_x;
+	double raydir_y;
+	int map_x;
+	int map_y;
+	
+	double delta_dist_x;
+	double delta_dist_y;
+	double side_dist_x;
+	double side_dist_y;
+	int step_x;
+	int step_y;
+
 	int side;
-	double rayDirX;
-	double rayDirY;
-	int lineHeight;
-	int drawStart;
-	int drawEnd;
+	double perp_wall_dist;
+	int wall_height;
+	int draw_start;
+	int draw_end;
+
+	double wall_x;
 } t_var;
 
-//error
-void	ft_error(char *msg);
-void	ft_free(char **list);
-int		is_two_word(char *line);
-int		is_three_word(char *line);
+// error
+void ft_error(char *msg);
+void ft_free(char **list);
+int is_two_word(char *line);
+int is_three_word(char *line);
 
-//parsing
-void	arg_n_init(int argc, char **argv, t_data *data, t_parsing *parse);
-void	check_direction(t_data *data, char dir);
-void	parsing(int argc, char **argv, t_data *data, t_parsing *parse);
+// parsing
+void arg_n_init(int argc, char **argv, t_data *data, t_parsing *parse);
+void check_direction(t_data *data, char dir);
+void parsing(int argc, char **argv, t_data *data, t_parsing *parse);
 
-//init_data
-int		check_file(char *file, t_parsing *parse);
-int		init_parse(t_parsing *parse);
-void	init_data(t_data *data);
-int		alloc_data(t_data *data, t_parsing *parse);
+// init_data
+int check_file(char *file, t_parsing *parse);
+int init_parse(t_parsing *parse);
+void init_data(t_data *data);
+int alloc_data(t_data *data, t_parsing *parse);
 
-//check_identifier
-int		check_image(t_data *data, t_parsing *parse);
-int		check_identifier(t_data *data, t_parsing *parse);
+// check_identifier
+int check_image(t_data *data, t_parsing *parse);
+int check_identifier(t_data *data, t_parsing *parse);
 
-//validate_map
-int		validate_map(t_data *data, t_parsing *parsing);
+// validate_map
+int validate_map(t_data *data, t_parsing *parsing);
 
-//set_map
-int		set_map(t_data *data, t_parsing *parse);
+// set_map
+int set_map(t_data *data, t_parsing *parse);
 
 // 구현부
-void	open_window(t_data *data);
-int		key_press(int keycode, t_data *data);
+void open_window(t_data *data);
+int key_press(int keycode, t_data *data);
 
 // void	set_value(t_data *d, t_var *v, int x);
 // void	set_value2(t_data *d, t_var *v);
--- srcs/execute/open_window.c
+++ srcs/execute/open_window.c
@@ -6,7 +6,7 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 01:47:18 by wonlim            #+#    #+#             */
-/*   Updated: 2023/08/08 21:48:59 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/08/13 02:48:56 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,26 +35,26 @@ void paint_cf(t_data *data)
 void draw_wall(int direction, t_var v, t_image *img, t_data *d, int x)
 {
 	// 비율에 너비를 곱해서 실제 texture의 좌표를 구함
-	int texX = (int)(v.wallX * (double)img->width);
+	int tex_x = (int)(v.wall_x * (double)img->width);
 
 	// 플레이어 시선이 위쪽이면 만약 비율이 0.2일 경우 제대로 좌에서 우로 갈 수록 증가하는데
-	// 위에서 아래를 보면 0.8인 부분을 0.2 라 생각하기 때문에 texX를 빼줘야 한다.
+	// 위에서 아래를 보면 0.8인 부분을 0.2 라 생각하기 때문에 tex_x를 빼줘야 한다.
 	if (direction == NO || direction == EA)
-		texX = img->width - texX ;
+		tex_x = img->width - tex_x ;
 
 	// 화면 픽셀당 텍스처 좌표를 얼마만큼 증가시킬지
-	double step = 1.0 * img->height / v.lineHeight;
+	double step = 1.0 * img->height / v.wall_height;
 
 	// todo y 시작 텍스처 좌표
-	double texPos = (v.drawStart - HEI / 2 + v.lineHeight / 2) * step;
+	double tex_pos = (v.draw_start - HEI / 2 + v.wall_height / 2) * step;
 
-	for (int y = v.drawStart; y < v.drawEnd; y++)
+	for (int y = v.draw_start; y < v.draw_end; y++)
 	{
 		// 텍스처 좌표를 정수로 변환하고 오버플로우의 경우 (texHeight - 1)로 마스크합니다.
-		int texY = (int)texPos & (img->height - 1);
-		texPos += step;
+		int tex_y = (int)tex_pos & (img->height - 1);
+		tex_pos += step;
 
-		int color = img->buffer[img->height * texY + texX];
+		int color = img->buffer[img->height * tex_y + tex_x];
 
 		d->image.buffer[x + y * WID] = color;
 		// d->re_buf = 1;
@@ -64,167 +64,119 @@ void draw_wall(int direction, t_var v, t_image *img, t_data *d, int x)
 void calc(t_data *d)
 {
 	int x;
-
+	
 	x = 0;
 
 	while (x < WID)
 	{
-		double cameraX = 2 * x / (double)WID - 1;			  // 카메라 평면 범위 변경 (-1 ~ 1)
-		double rayDirX = d->dir_x + d->plane_x * cameraX; // 광선의 방향 벡터
-		double raydir_y = d->dir_y + d->plane_y * cameraX; // 광선의 방향 벡터
+		double camera_x = 2 * x / (double)WID - 1;			  // 카메라 평면 범위 변경 (-1 ~ 1)
+		double raydir_x = d->dir_x + d->plane_x * camera_x; // 광선의 방향 벡터
+		double raydir_y = d->dir_y + d->plane_y * camera_x; // 광선의 방향 벡터
 
-		int mapX = (int)d->pos_x; // 플레이어(광선)의 위치
-		int mapY = (int)d->pos_y; // 플레이어(광선)의 위치
+		int map_x = (int)d->pos_x; // 플레이어(광선)의 위치
+		int map_y = (int)d->pos_y; // 플레이어(광선)의 위치
 
-		double deltaDistX = fabs(1 / rayDirX);
-		double deltaDistY = fabs(1 / raydir_y);
+		double delta_dist_x = fabs(1 / raydir_x);
+		double delta_dist_y = fabs(1 / raydir_y);
 
-		double sideDistX, sideDistY;
+		double side_dist_x, side_dist_y;
 
 		// x, y가 어느 방향으로 이동하는지(광선의 방향)
 		// -1 또는 +1 의 값을 가진다.
-		int stepX, stepY;
+		int step_x, step_y;
 
-		/*
-		stepX, stepY
-		광선의 방향이 음수 x 성분을 가지면, `stepX = -1`, 광선의 방향이 양수 x 성분을 가지면 `stepX = 1`.
-		만약 x 성분이 0이면, stepX 값은 사용되지 않으므로 어떤 값이든 상관없다.
-		y 도 동일하게 처리한다.
-		*/
-
-		if (rayDirX < 0)
+		if (raydir_x < 0)
 		{
-			stepX = -1;
-			sideDistX = (d->pos_x - mapX) * deltaDistX;
+			step_x = -1;
+			side_dist_x = (d->pos_x - map_x) * delta_dist_x;
 		}
 		else
 		{
-			stepX = 1;
-			sideDistX = (mapX + 1.0 - d->pos_x) * deltaDistX;
+			step_x = 1;
+			side_dist_x = (map_x + 1.0 - d->pos_x) * delta_dist_x;
 		}
 		if (raydir_y < 0)
 		{
-			stepY = -1;
-			sideDistY = (d->pos_y - mapY) * deltaDistY;
+			step_y = -1;
+			side_dist_y = (d->pos_y - map_y) * delta_dist_y;
 		}
 		else
 		{
-			stepY = 1;
-			sideDistY = (mapY + 1.0 - d->pos_y) * deltaDistY;
+			step_y = 1;
+			side_dist_y = (map_y + 1.0 - d->pos_y) * delta_dist_y;
 		}
 
 		// 나중에 광선의 길이를 계산하는 데 사용되는 변수
-		double perpWallDist;
+		double perp_wall_dist;
 
 		int hit = 0; // 현재 루프를 종료할지 여부를 결정하는 데 사용되는 변수
 		int side;	 // 벽의 x면 또는 y면이 맞았는지를 나타내는 변수 (x = 0 , y = 1)
 
-		/*
-		광선이 벽에 도달하면 루프가 종료된다.
-
-		sideDistX, sideDistY
-		각 이동 방향마다 deltaDist만큼 증가한다.
-
-		mapX, mapY
-		각각 stepX와 stepY만큼 증가한다.
-
-		그 시점에서 "side" 변수에는 벽의 x면 또는 y면이 맞았는지 여부가 저장되며, mapX와 mapY에는 어떤 벽이 맞았는지에 대한 정보가 저장된다.
-		그러나 벽이 정확히 어디에서 맞았는지는 알 수 없지만, 현재는 텍스처된 벽을 사용하지 않기 때문에 이 정보는 필요하지 않습니다.
-		*/
-		// perform DDA
+		
 		while (hit == 0)
 		{
-			// jump to next map square, OR in x-direction, OR in y-direction
-			if (sideDistX < sideDistY)
+		
+			if (side_dist_x < side_dist_y)
 			{
-				sideDistX += deltaDistX;
-				mapX += stepX;
+				side_dist_x += delta_dist_x;
+				map_x += step_x;
 				side = 0;
 			}
 			else
 			{
-				sideDistY += deltaDistY;
-				mapY += stepY;
+				side_dist_y += delta_dist_y;
+				map_y += step_y;
 				side = 1;
 			}
-			// Check if ray has hit a wall
-			if (d->map[mapY][mapX] == '1')
+			
+			if (d->map[map_y][map_x] == '1')
 				hit = 1;
 		}
 
-		// 카메라 방향에 투영된 거리를 계산. This is the shortest distance from the point where the wall is
-		// hit to the camera plane. Euclidean to center camera point would give fisheye effect!
-		// This can be computed as (mapX - pos_x + (1 - stepX) / 2) / rayDirX for side == 0, or same formula with Y
-		// for size == 1, but can be simplified to the code below thanks to how sideDist and deltaDist are computed:
-		// because they were left scaled to |rayDir|. sideDist is the entire length of the ray above after the multiple
-		// steps, but we subtract deltaDist once because one step more into the wall was taken above.
 		if (side == 0)
-			perpWallDist = (sideDistX - deltaDistX);
+			perp_wall_dist = (side_dist_x - delta_dist_x);
 		else
-			perpWallDist = (sideDistY - deltaDistY);
+			perp_wall_dist = (side_dist_y - delta_dist_y);
 
-		// Calculate height of line to draw on screen
-		int lineHeight = (int)(HEI / perpWallDist);
+		int wall_height = (int)(HEI / perp_wall_dist);
 
-		// calculate lowest and highest pixel to fill in current stripe
-		int drawStart = -lineHeight / 2 + HEI / 2;
-		if (drawStart < 0)
-			drawStart = 0;
-		int drawEnd = lineHeight / 2 + HEI / 2;
-		if (drawEnd >= HEI)
-			drawEnd = HEI - 1;
+		
+		int draw_start = -wall_height / 2 + HEI / 2;
+		if (draw_start < 0)
+			draw_start = 0;
+		int draw_end = wall_height / 2 + HEI / 2;
+		if (draw_end >= HEI)
+			draw_end = HEI - 1;
 
-		// ray가 부딫힌 벽 실제 좌표
-		double wallX;
+		
+		double wall_x;
 
 		if (side == 0)
-			wallX = d->pos_y + perpWallDist * raydir_y;
+			wall_x = d->pos_y + perp_wall_dist * raydir_y;
 		else
-			wallX = d->pos_x + perpWallDist * rayDirX;
-		wallX -= floor(wallX);
+			wall_x = d->pos_x + perp_wall_dist * raydir_x;
+		wall_x -= floor(wall_x);
 
 
 		t_var var;
-		var.wallX = wallX;
+		var.wall_x = wall_x;
 		var.side = side;
-		var.rayDirX = rayDirX;
-		var.rayDirY = raydir_y;
-		var.lineHeight= lineHeight;
-		var.drawStart=drawStart;
-		var.drawEnd = drawEnd;
+		var.raydir_x = raydir_x;
+		var.raydir_y = raydir_y;
+		var.wall_height= wall_height;
+		var.draw_start=draw_start;
+		var.draw_end = draw_end;
 
-		if (side == 0 && rayDirX > 0) // 서
+		if (side == 0 && raydir_x > 0) // 서
 			draw_wall(WE, var, &d->images[WE], d, x);
 		else if (side == 1 && raydir_y < 0) // 남
 			draw_wall(SO, var, &d->images[SO], d, x);
-		else if (side == 0 && rayDirX <= 0) // 동
+		else if (side == 0 && raydir_x <= 0) // 동
 			draw_wall(EA, var, &d->images[EA], d, x);
 		else // 북
 			draw_wall(NO, var, &d->images[NO], d, x);
 		x++;
 	}
-	/*
-	int x;
-	t_var v;
-
-	x = 0;
-	while (x < WID)
-	{
-		set_value(d, &v, x);
-		set_value2(d, &v);
-		dda(d, &v);
-		get_wall_area(d, &v);
-		if (v.side == 0 && v.raydir_x > 0)
-			draw_wall(WE, v, d, x);
-		else if (v.side == 1 && v.raydir_y < 0)
-			draw_wall(SO, v, d, x);
-		else if (v.side == 0 && v.raydir_x <= 0)
-			draw_wall(EA, v, d, x);
-		else
-			draw_wall(NO, v, d, x);
-		x++;
-	}
-	*/
 }
 
 int main_loop(t_data *data)
