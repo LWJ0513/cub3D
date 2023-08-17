d0cd192420ab4b42c618455c8454a5290877165a
ppparkta <pppppark2@gmail.com>
2023-08-17 16:38:00 +0900

    fix: leak 검사

--- srcs/main.c
+++ srcs/main.c
@@ -3,22 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: sooyang <sooyang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 21:16:48 by wonlim            #+#    #+#             */
-/*   Updated: 2023/08/17 13:59:04 by sooyang          ###   ########.fr       */
+/*   Updated: 2023/08/17 16:37:29 by sooyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	leaks(){
-	system("leaks cub3D");
-}
-
 int	main(int argc, char **argv)
 {
-	atexit(leaks);	
 	t_data		data;
 	t_parsing	parse;
 
